9b6c0c57d7a8d4a6976cb1ae873795498855786d
Tesfa Bizuneh <41780689+tbizuneh@user.noreply.github.com>
2019-01-30 17:50:20 -0500

    Updates

---
+++ precache-manifest.34bd3281b229f2c94bb0229c8a17db15.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0037537bc232f70c47ecfea1a686b05",
+    "url": "/catsclickygame/static/media/kitty3.b0037537.jpg"
+  },
+  {
+    "revision": "5066e97e79ac503c1a0e",
+    "url": "/catsclickygame/static/css/main.302193df.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/catsclickygame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "7bb281b4c2e43f9519da",
+    "url": "/catsclickygame/static/js/runtime~main.7bb281b4.js"
+  },
+  {
+    "revision": "a1abe1985db455ed2441e01d96db40da",
+    "url": "/catsclickygame/static/media/kitty1.a1abe198.jpeg"
+  },
+  {
+    "revision": "af5202169f35cb3bb4c7a56684566488",
+    "url": "/catsclickygame/static/media/kitty12.af520216.jpg"
+  },
+  {
+    "revision": "c99dfa0d7c73a684c53a94ea19ea6135",
+    "url": "/catsclickygame/static/media/kitty2.c99dfa0d.jpg"
+  },
+  {
+    "revision": "5066e97e79ac503c1a0e",
+    "url": "/catsclickygame/static/js/main.5066e97e.chunk.js"
+  },
+  {
+    "revision": "5db206e5ea2bbae0d5c150aece2a783c",
+    "url": "/catsclickygame/static/media/kitty4.5db206e5.jpg"
+  },
+  {
+    "revision": "9bf939deadd9986309bfe1ef0d18e198",
+    "url": "/catsclickygame/static/media/kitty5.9bf939de.jpeg"
+  },
+  {
+    "revision": "36c30621b487d1e28a1f4ae6680ae9c8",
+    "url": "/catsclickygame/static/media/kitty6.36c30621.jpeg"
+  },
+  {
+    "revision": "a529b241cd20b3584763a3ad5f04dcf1",
+    "url": "/catsclickygame/static/media/kitty7.a529b241.jpeg"
+  },
+  {
+    "revision": "11c385dca598f03df4b33c505e42b2c8",
+    "url": "/catsclickygame/static/media/kitty8.11c385dc.jpg"
+  },
+  {
+    "revision": "e0e2f54c6ecb7addb12eb2b9b6aa0839",
+    "url": "/catsclickygame/static/media/kitty9.e0e2f54c.jpg"
+  },
+  {
+    "revision": "6b3c77bc68222e5042ed729e970fd469",
+    "url": "/catsclickygame/index.html"
+  }
+];
